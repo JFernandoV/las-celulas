204f5f2584b69db8a7eb2dfa2d026c2719303185
FernadoV <69281864+FernadoV@users.noreply.github.com>
2020-08-06 11:24:54 -0600

    dcwc

---
+++ prueba.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<p>&lt;?php</p>
+<p>$i=5;</p>
+<p>$i++;</p>
+<p>&nbsp;</p>
+<p>echo &quot;hola&quot;.$i;</p>
+<p>&nbsp;</p>
+<p>?&gt;</p>
+</body>
+</html>
